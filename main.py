d610679d688ff1816ba1db0b4b23d9e23dff6cb0
lukelev07 <lukelev07@gmail.com>
2014-12-04 21:11:41 -0800

    small fix

--- main.py
+++ main.py
@@ -22,22 +22,22 @@ def classify(x, t_tupes):
 # open training data
 training_set_file = open('hw12data/digitsDataset/trainFeatures.csv','r')
 training_set_iter = csv.reader(training_set_file)
-training_set = [float(item) for item in training_set_iter]
+training_set = [item for item in training_set_iter]
 
 # open labels
 training_label_file = open('hw12data/digitsDataset/trainLabels.csv','r')
 training_label_iter = csv.reader(training_label_file)
-training_label = [float(item) for item in training_label_iter]
+training_label = [item for item in training_label_iter]
 
 # pair label with training data
 t_set = []
 for index, val in enumerate(training_label):
-    t_set.append((val, training_set[index]))
+    t_set.append((int(val[0]), [float(item) for item in training_set[index]]))
 
 # open vals
 val_file = open('hw12data/digitsDataset/valFeatures.csv','r')
 val_iter = csv.reader(val_file)
-val_set = [float(item) for item in val_iter]
+val_set = [int(item[0]) for item in val_iter]
 
 for image in val_set:
     print(classify(image, t_set))
